934e551902d4fd2596b64c73852fecd6139cdc92
Konstantin <loukianen@gmail.com>
2022-10-18 11:45:57 +0300

    first part minus

--- 03_buyAndSell.js
+++ 03_buyAndSell.js
@@ -3,34 +3,41 @@
 // const content = fs.readFileSync(__dirname + '/input.txt', 'utf-8');
 
 // const getData = (str) => {
-//     const [, arrStr] = content.split('\n');
+//     const [, arrStr] = str.split('\n');
 //     return arrStr ? arrStr.split(' ') : [];
 // };
+
+const money = 1;
 
 // const res = getBaySellDays(getData(content));
 // console.log(res[0], res[1]);
 
 
 function getBaySellDays(prices) {
-    console.log(prices);
     let income = 0;
     let buyDay = 0;
     let sellDay = 0;
+    let gazVolume = 0;
 
-    for (i = 1; i < prices.length; i += 1) {
-        for (j = i + 1; j <= prices.length; j += 1) {
-            const currentIncom = prices[j - 1] - prices[i - 1];
-            if (currentIncom > income) {
-                income = currentIncom;
-                buyDay = i;
-                sellDay = j;
-            }
+    let middleBuyDay = 0;
+
+    for (i = 0; i < prices.length; i += 1) {
+        const currentGazVolume = money / prices[i];
+        if(currentGazVolume > gazVolume) {
+            gazVolume = currentGazVolume;
+            middleBuyDay = i + 1;
+        }
+        const currentIncom = prices[i] * gazVolume - money;
+        if (currentIncom > income) {
+            income = currentIncom;
+            sellDay = i + 1;
+            buyDay = middleBuyDay;
         }
     }
     return [buyDay, sellDay];
 }
 
-const testData = [[[6, [10, 3, 5, 3, 11, 9]], [2, 5]], [[4, [5, 5, 5, 5]], [0, 0]], [[1, [10]], [0, 0]], [[0, []], [0, 0]]];
+const testData = [[[6, [10, 3, 5, 3, 11, 9]], [2, 5]], [[4, [5, 5, 5, 5]], [0, 0]], [[1, [10]], [0, 0]], [[0, []], [0, 0]], [[4, [5, 7, 4, 5]], [1, 2]], [[4, [5, 7, 6, 10]], [1, 4]]];
 
 testData.forEach(([input, res]) => {
     console.log('input: ', input, 'rr: ', res, 'fact: ', getBaySellDays(input[1]));
--- 05_breakPalindrome.js
+++ 05_breakPalindrome.js
@@ -5,41 +5,21 @@ const content = fs.readFileSync(__dirname + '/input.txt', 'utf-8');
 const res = breakPalindrome(content);
 console.log(res);
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const letters = alphabet.reduce((acc, letter, index) => {
-    acc[letter] = index;
-    return acc;
-}, {});
-
 function breakPalindrome(palindrome) {
-    const palindromeMiddleIndex = Math.floor((palindrome.length - 1) / 2) + 1;
-    if (palindromeMiddleIndex < 1) {
+    const palindromeMiddleIndex = Math.ceil((palindrome.length - 1) / 2);
+    if (palindrome.length < 2) {
         return '';
     }
-    const palindromePartForChange = palindrome.slice(0, palindromeMiddleIndex);
-    for (let i = palindromeMiddleIndex; i >= 0; i -= 1) {
-        const lastLetter = palindromePartForChange[i];
-        const newLetter = alphabet[letters[lastLetter] - 1];
-        if (newLetter) {
-            const curStr = palindromePartForChange.slice(0, i).concat(newLetter, palindromePartForChange.slice(i + 1));
-            if (curStr < palindromePartForChange) {
-                return curStr.concat(palindrome.slice(palindromeMiddleIndex));
-            }
+    for (let i = 0; i < palindromeMiddleIndex; i += 1) {
+        if (palindrome[i] !== 'a') {
+            return palindrome.slice(0, i).concat('a', palindrome.slice(i + 1));
         }
     }
-    return '';
+    return palindrome.slice(0, -1).concat('b');
 }
 
-// const testData = [['abba', 'aaba'], ['a', ''], ['aza', 'aya']];
+// const testData = [['abba', 'aaba'], ['a', ''], ['aazzaa', 'aaazaa'], ['aazaa', 'aazab'], ['aaaaa', 'aaaab'], ['aa', 'ab']];
 
 // testData.forEach(([input, res]) => {
 //     console.log('input: ', input, 'rr: ', res, 'fact: ', breakPalindrome(input));
 // });
-
-
-// for (let i = 0; i < palindromeMiddleIndex; i += 1) {
-//     const curStr = palindromePartForChange.slice(0, i).concat('a', palindromePartForChange.slice(i + 1));
-//     if (curStr < palindromePartForChange) {
-//         return curStr.concat(palindrome.slice(palindromeMiddleIndex));
-//     }
-// }
